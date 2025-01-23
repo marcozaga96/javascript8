7775e22dabd9feb47061e9ad2c5c45b0557d1ea3
Marco Zagaria <marcozagaria@live.com>
2025-01-23 19:23:37 +0100

    fine

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const numeri = [];
 const tombolone = document.getElementById("tombolone");
+const cartelle = document.getElementById("cartelle");
+const singleCartella = [];
 for (let index = 1; index <= 76; index++) {
-  tombolone.innerHTML += `<div id="${index}" class='cell'>${index}</div>`;
+  tombolone.innerHTML += `<div id="tombolone-${index}" class='cell'>${index}</div>`;
+}
+for (let index = 1; index <= 24; index++) {
+  let numero = Math.floor(Math.random() * 76) + 1;
+  if (!singleCartella.includes(numero)) {
+    singleCartella.push(numero);
+    cartelle.innerHTML += `<div id="cartella-${numero}" class='cell'>${numero}</div>`;
+  }
 }
 
 const colora = () => {
   let estratto;
   let trovato = false;
-  for (let index = 0; index < 76; index++) {
+  for (let index = 1; index < 76; index++) {
     estratto = Math.floor(Math.random() * 76) + 1;
     if (!numeri.includes(estratto)) {
       trovato = true;
@@ -18,7 +27,11 @@ const colora = () => {
     numeri.push(estratto);
     document.getElementById("casuale").innerHTML =
       "Numero estratto " + estratto;
-    document.getElementById(estratto).classList.add("colora");
-    console.log(numeri);
-  } else alert("tutti i numeri sono stati estratti");
+    document.getElementById(`tombolone-${estratto}`).classList.add("colora");
+    if (singleCartella.includes(estratto)) {
+      document.getElementById(`cartella-${estratto}`).classList.add("colora");
+    }
+  } else {
+    alert("Tutti i numeri sono stati estratti");
+  }
 };
